3da260e6782b28d3e46abe5530d9d1e5797037b4
unknown <olaniyigeorge77@gmail.com>
2024-02-27 14:00:04 +0100

    Restyling for mobile'

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,18 +5,18 @@ import { Link } from "@remix-run/react";
 export default function Header() {
 
 
-    return <header className="flex justify-between items-center w-full h-max-20  py-3 px-8 shadow">
-        <Link to="/" className="text-white font-extrabold text-5xl font-mono">
+    return <header className="flex justify-between items-center w-full h-max-20  py-3 px-4 sm:px-8 shadow">
+        <Link to="/" className="text-white font-extrabold text-2xl md:text-5xl font-mono">
             RASP
         </Link>
 
         <span className="text-white space-x-4">
-            <Link to="/about-us"> About </Link>
-            <Link to="/blog"> Blog </Link>
-            <Link to="/contact"> Contact</Link>
+            <Link className="p-2 text-xs md:text-md hover:underline transition-all" to="/about-us"> About </Link>
+            <Link className="p-2 text-xs md:text-md hover:underline transition-all" to="/blog"> Blog </Link>
+            <Link className="p-2 text-xs md:text-md hover:underline transition-all" to="/contact"> Contact</Link>
         </span>
 
-        <span className="text-white">
+        <span className="text-xs md:text-md text-white">
             <p>Get Started</p>
         </span>
 

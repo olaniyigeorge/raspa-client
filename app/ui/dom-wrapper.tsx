f4ba8e8cb47e858d79d813c5c1bbd9243ce8bde6
unknown <olaniyigeorge77@gmail.com>
2024-03-01 04:38:37 +0100

    DOmWrapper

---
+++ app/ui/dom-wrapper.tsx
@@ -0,0 +1,42 @@
+
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react'
+import  { AnimatePresence } from 'framer-motion'
+
+
+// import { ColourSchemeProvider, TColourScheme } from '~/ui/colour-scheme'
+
+export function DomWrapper(props: IDomWrapper) {
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+
+        <AnimatePresence mode="wait"><Outlet /></AnimatePresence>
+
+        {props.scripts}
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  )
+}
+
+interface IDomWrapper {
+  children: React.ReactNode
+  scripts?: React.ReactNode
+}

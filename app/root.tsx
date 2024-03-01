352e8dc82309fcb26e0626eadd5e0db32344e88f
unknown <olaniyigeorge77@gmail.com>
2024-03-01 04:41:02 +0100

    Added persistent user context management

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,4 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import { json, LinksFunction, LoaderFunctionArgs, redirect } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,8 +6,15 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 import stylesheet from "~/styles/tailwind.css";
+import { fetchNewAccessToken, UserContextProvider } from "./api/auth";
+import { UserContext } from "./api/interfaces";
+import { getUrl } from "./api/util";
+import { getContext, hasAccess } from "./auth.server";
+import { currentRefreshToken, currentToken, enableAuthenticableServerApiRequests, storage } from "./session.server";
+import { DomWrapper } from "./ui/dom-wrapper";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
@@ -16,20 +22,104 @@ export const links: LinksFunction = () => [
 ];
 
 export default function App() {
+  const data = useLoaderData<typeof loader>()
+  
+
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+    <DomWrapper
+      scripts={
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.ENV = ${JSON.stringify(data?.ENV)}`,
+          }}
+        />
+      }
+    >
+      <UserContextProvider usrContext={data?.userContext}>
+        <AnimatePresence mode="wait">
+          <Outlet />
+        </AnimatePresence>
+      </UserContextProvider>
+    </DomWrapper>
+  )
+    };
+
+
+
+
+
+export async function loader({request,}: LoaderFunctionArgs){
+  // --------
+  console.log("Loading.......")
+  
+  let userContext: UserContext | null = null;
+  const curntToken = await currentToken(request);
+
+  // console.log(`Got token ${curntToken}`)
+  if (!curntToken || curntToken == 'anonymous') {
+    // Token is anonymous, set userContext to null
+    userContext = null
+  }
+
+  //console.log("Check token validity");
+  try {
+    // Check if the current token is still valid
+    // console.log("Confirming access------");
+    const accessible = await hasAccess(request);
+    //console.log(`Has access: ${accessible}`)
+
+    if (accessible == '1') {
+      userContext = await getContext(request)
+    }
+    else if (accessible == '2') {
+      // Token validation failed with 400 status, attempt fetching new access token
+      //console.log("Attempting token refresh.........")
+      const refreshToken = await currentRefreshToken(request);
+      
+      if (refreshToken && refreshToken !== 'anonymous') {
+        const fetchNewAccessResponse = await fetchNewAccessToken(refreshToken);
+
+        if (fetchNewAccessResponse === null) {
+          return "0";
+        }
+
+        //Use the new access token in subsequent requests if needed
+        const session = await enableAuthenticableServerApiRequests(request, {
+          token: fetchNewAccessResponse,
+          refresh: refreshToken,
+        });
+
+        return redirect("/", {
+          headers: {
+            "Set-Cookie": await storage.commitSession(session),
+          },
+        });
+      }
+    }
+    else {
+      //console.log("No access, user is null")
+      userContext = null
+    }
+    
+  } catch (error) {
+    //console.log("Error getting context")
+    userContext = null
+
+  }
+
+
+  
+
+  const ENV = {
+    APP_COOKIE_NAME: process.env.APP_COOKIE_NAME,
+    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+    APP_SECRET: process.env.APP_SECRET,
+
+  }
+
+  const context_ep = getUrl('context');
+  return json({
+    userContext,
+    ENV,
+  })
 }

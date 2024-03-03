b3bf58b7806eda5bff7a08fe7f3930c752bce21c
unknown <olaniyigeorge77@gmail.com>
2024-03-03 15:35:36 +0100

    restyled the landing hero

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,22 +14,22 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export default function Index() {
+export default function Landing() {
   const navigate = useNavigate();
   const [selectedButton, setSelectedButton] = useState<string>('buy');
   const [searchQuery, setSearchQUery] = useState<string>("")
   const submit = useSubmit()
   console.log(selectedButton)
   console.log(searchQuery)
+  
   function handleSearch() {
     console.log("Handling search")
 
     try {
-      const formData = new FormData()
-      // if selectedButton not in ['buy','sell','invest','rent'] raise error
-      // if 
-      formData.append('action', selectedButton)
-      formData.append('search', searchQuery)
+      const formData = new FormData();
+      // TODO if selectedButton not in ['buy','sell','invest','rent'] raise error
+      const selectedButton = (formData.get("action") || "").toString();
+      const searchQuery = (formData.get("search") || "").toString();
       console.log("Values appended to formData")
 
       // submit(formData, { method: 'post', replace: true })
@@ -39,9 +39,9 @@ export default function Index() {
       // console log constructed url 
       // ....and redirect to it
     
-      return navigate(en, { replace: true })
+      return redirect(en);
     } catch {
-      console.log("Error submitting form to action")
+      console.log("Error submitting form")
     }
   }
 
@@ -52,78 +52,80 @@ export default function Index() {
   
   return (
     <div className="w-full h-full">
-      <div id="Land" className="w-full h-auto md:h-screen bg-purple-500  bg-blend-multiply  bg-[url('/images/rendering-house-model.png')] ">
-    
+      <div id="Land" className="w-full h-auto md:h-screen  bg-blend-multiply  bg-[url('/images/rendering-house-model.png')] ">
+        <div className="w-full h-full bg-gradient-to-r from-purple-800 bg-blend-overlay  big-[url('/images/landed-properties.png')">
+
           <Header />
 
-        <div className="w-full h-auto">
-          <div className="mt-20 md:mt-36 md:ml-10 items-start space-y-4">
-            <h1 className="w-full md:w-2/3  text-center md:text-start  text-3xl sm:text-7xl font-extrabold text-white ">
-              Discover Spaces That Suit Your Needs
-            </h1>
-            <h2 className="mx-2 text-white text-center md:text-start  text-xl md:text-2xl font-medium">
-              Connecting renters, buyers, and investors to 
-              available properties
-            </h2>
-          </div>
-          
-
-          <div className="w-full flex justify-center p-2 h-16 mt-4">
-            <div className="w-4/6 md:w-1/2 p-2 flex items-center space-x-2 rounded-full" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)'}}>
-              <button
-                className={`p-2 flex-grow justify-center items-center rounded-full transition-all ${
-                  selectedButton === 'buy' ? 'bg-white shadow-md font-bold' : ''
-                }`}
-                onClick={() => handleButtonClick('buy')}
-              >
-                Buy
-              </button>
-              <button
-                className={`p-2 flex-grow justify-center items-center rounded-full transition-all ${
-                  selectedButton === 'sell' ? 'bg-white shadow-md font-bold' : 'bg-transparent'
-                }`}
-                onClick={() => handleButtonClick('sell')}
-              >
-                Sell
-              </button>
-              <button
-                className={`p-2  flex-grow justify-center items-center rounded-full transition-all ${
-                  selectedButton === 'rent' ? 'bg-white shadow-md font-bold' : 'bg-transparent'
-                }`}
-                onClick={() => handleButtonClick('rent')}
-              >
-                Rent
-              </button>
-              <button
-                className={`p-2 flex-grow justify-center items-center rounded-full transition-all ${
-                  selectedButton === 'invest' ? 'bg-white shadow-md font-bold' : 'bg-transparent'
-                }`}
-                onClick={() => handleButtonClick('invest')}
-              >
-                Invest
-              </button>
+          <div className="w-full h-auto">
+            <div className="mt-20 md:mt-36 md:ml-10 items-start space-y-4">
+              <h1 className="w-full md:w-2/3  text-center md:text-start  text-3xl sm:text-7xl font-extrabold text-white ">
+                Discover Spaces That Suit Your Needs
+              </h1>
+              <h2 className="mx-2 text-white text-center md:text-start  text-xl md:text-2xl ">
+                Connecting renters, buyers, and investors to 
+                available properties
+              </h2>
             </div>
-          </div>   
+            
 
-          <div id="search-bar" className="w-full flex justify-center mt-4 p-2 h-16">
-            <form  className="w-5/6 md:w-2/3 flex items-center space-x-2  rounded-full">
-            <input type="text" onChange={(e) => {setSearchQUery(e.target.value)}} className="w-full h-full flex-1 p-3 ring-0 active:ring-0 rounded-full active:border-none" placeholder="Search for street name, city, property type, zip code... "/>
-            <button
-              className="border-2 text-xs md:text-md border-white shadow-md bg-purple-600 rounded-full text-white text-bold h-full  p-2 md:px-5 "
-              // On click contruct a url with search params and useNavigate to it
-              onClick={() => handleSearch()}
-            >
-              Search
-            </button>
-            </form>
-          </div> 
-        </div>
+            <div className="w-full flex justify-center p-2 h-16 mt-4">
+              <div className="w-4/6 md:w-1/2 p-2 flex items-center space-x-2 rounded-full" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)'}}>
+                <button
+                  className={`p-2 flex-grow justify-center items-center rounded-full transition-all ${
+                    selectedButton === 'buy' ? 'bg-white shadow-md font-bold' : ''
+                  }`}
+                  onClick={() => handleButtonClick('buy')}
+                >
+                  Buy
+                </button>
+                <button
+                  className={`p-2 flex-grow justify-center items-center rounded-full transition-all ${
+                    selectedButton === 'sell' ? 'bg-white shadow-md font-bold' : 'bg-transparent'
+                  }`}
+                  onClick={() => handleButtonClick('sell')}
+                >
+                  Sell
+                </button>
+                <button
+                  className={`p-2  flex-grow justify-center items-center rounded-full transition-all ${
+                    selectedButton === 'rent' ? 'bg-white shadow-md font-bold' : 'bg-transparent'
+                  }`}
+                  onClick={() => handleButtonClick('rent')}
+                >
+                  Rent
+                </button>
+                <button
+                  className={`p-2 flex-grow justify-center items-center rounded-full transition-all ${
+                    selectedButton === 'invest' ? 'bg-white shadow-md font-bold' : 'bg-transparent'
+                  }`}
+                  onClick={() => handleButtonClick('invest')}
+                >
+                  Invest
+                </button>
+              </div>
+            </div>   
+
+            <div id="search-bar" className="w-full flex justify-center mt-4 p-2 h-16">
+              <form  className="w-5/6 md:w-2/3 flex items-center space-x-2  rounded-full">
+              <input type="text" onChange={(e) => {setSearchQUery(e.target.value)}} className="w-full h-full flex-1 p-3 ring-0 active:ring-0 rounded-full active:border-none" placeholder="Search for street name, city, property type, zip code... "/>
+              <button
+                className="border-2 text-xs md:text-md border-white shadow-md bg-purple-600 rounded-full text-white text-bold h-full  p-2 md:px-5 "
+                // On click contruct a url with search params and useNavigate to it
+                onClick={() =>  handleSearch()}
+              >
+                Search
+              </button>
+              </form>
+            </div> 
+          </div>
 
           <div className="w-full flex mb-10 md:mb-0 justify-end">
               <Link to="/#property-types" className="flex justify-center items-center mr-5 rounded-full p-3 text-white text-xl hover:text-purple-600 hover:bg-white">
                   ^
               </Link>
           </div>
+        </div>
       </div>
 
       <div id="property-types" className="flex-col justify-center mt-8">

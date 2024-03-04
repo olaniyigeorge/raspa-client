05d441625e15756fc7a9a22538ad2637092f84c2
unknown <olaniyigeorge77@gmail.com>
2024-03-04 01:10:28 +0100

    Added unknown user pfp

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,3 +1,4 @@
+import { UserIcon } from "@heroicons/react/24/solid";
 import { Link } from "@remix-run/react";
 
 
@@ -5,9 +6,9 @@ import { Link } from "@remix-run/react";
 export default function Header() {
 
 
-    return <header className="flex justify-between items-center w-full h-max-20  py-3 px-4 sm:px-8 shadow">
-        <Link to="/" className="text-white font-extrabold text-2xl md:text-5xl font-mono">
-            RASP
+    return <header className="flex justify-between items-center w-full h-auto  py-3 px-4 sm:px-8 shadow">
+        <Link to="/" className="text-white">
+            <img className="h-10 w-auto" src="images/rasp-logo-white.png" alt="rasp" />
         </Link>
 
         <span className="text-white space-x-4">
@@ -17,9 +18,13 @@ export default function Header() {
             <Link className="  sm:text-md transition-ease hover:border-b transition-all" to="/explore/map/?action=buy&search="> Explore</Link>
         </span> 
 
-        <span className="sm:text-md text-white">
-            <p>Get Started</p>
+        <span className="flex items-center ">
+            <Link to="/login" className="mx-2 h-auto flex  text-sm items-start text-start text-white ">Get Started</Link>
+            <span className="w-8 h-8 border-2 border-black rounded-full p-1">
+                <UserIcon className="w-full h-full text-black" />
+            </span>
         </span>
+
 
 
     </header>

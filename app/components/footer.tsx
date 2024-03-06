7b47ed4f8bfde16b82138d2ca23eb6bf16982313
unknown <olaniyigeorge77@gmail.com>
2024-03-06 09:30:43 +0100

    Added footer compoenent and images

---
+++ app/components/footer.tsx
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+export default function Footer() {
+
+
+
+    return <>
+        <div className=""> 
+            Footer Links
+        </div>
+
+        <div className="w-full h-auto"> 
+        
+            <img src="images/rasp-footer-image.png" alt="rasp-service" className="w-full h-auto" />
+        
+        </div>
+    </>
+
+}

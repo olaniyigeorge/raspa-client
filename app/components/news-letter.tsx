0ddbdb9c5848ef055dac26e6474c979cca6ba57f
unknown <olaniyigeorge77@gmail.com>
2024-03-06 15:04:46 +0100

    Updates styles on different screens

--- app/components/news-letter.tsx
+++ app/components/news-letter.tsx
@@ -9,9 +9,9 @@ export default function NewsLetterForm() {
     return <div className="w-full flex justify-center">
         
         <div className="bg-white flex w-2/3 items-center border rounded-full space-x-1 p-1">
-            <NewspaperIcon className="text-purple-600 w-8 h-8 " />
-            <input type="text" placeholder="Enter your email" className="focus:outline-none focus:ring-0 active:border-0 w-full h-full p-2" />
-            <input type="submit" className="bg-purple-600 hover:bg-purple-700 text-white py-2 px-3 rounded-full" />
+            <NewspaperIcon className="text-purple-600 w-8 h-8 p-1 " />
+            <input type="text" placeholder="Enter your email" className="text-gray-800 focus:outline-none focus:ring-0 active:border-0 w-full h-full p-2" />
+            <input type="submit" className="bg-purple-600 text-sm  hover:bg-purple-700 text-white py-2 px-3 rounded-full" />
         </div>
     </div>
 }

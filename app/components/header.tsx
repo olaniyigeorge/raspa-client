55610a3fbe2e2164acb03c66c1b0017e14e7324e
unknown <olaniyigeorge77@gmail.com>
2024-03-18 06:19:50 +0100

    added animation to menu dropdown

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,6 +1,7 @@
 import { Bars3Icon } from "@heroicons/react/24/outline";
 import { UserIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import { Link } from "@remix-run/react";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
 interface IHeader {
@@ -18,7 +19,7 @@ export default function Header(props: IHeader) {
                 <img className="h-10 w-auto drop-shadow" src="images/rasp-logo-white.png" alt="rasp" />
             </Link>
 
-            <span className="hidden md:flex  text-xs md:text-md space-x-1 md:space-x-4">
+            <span className="hidden md:flex space-x-1 md:space-x-4">
                 <Link className="transition ease-in-out delay-100 hover:border-b " to="/about-us"> About </Link>
                 <Link className="transition ease-in-out delay-100 hover:border-b " to="/blog"> Blog </Link>
                 <Link className="transition ease-in-out delay-100 hover:border-b " to="/contact"> Contact</Link>
@@ -53,21 +54,31 @@ export default function Header(props: IHeader) {
 
         </div>
 
-        <div className={`${dropdownOpen ? 'block' : 'hidden' } w-full h-auto z-20 `} style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)'}}>
-                <div className="w-full py-2 px-4 flex justify-between  items-center">
-                    <Link to="/login" className="h-auto flex  text-xs md:text-md items-start text-start font-medium text-purple-600 ">Get Started</Link>
-                    <span className="w-8 h-8 border-2 border-black rounded-full p-1">
-                        <UserIcon className="w-full h-full text-black" />
-                    </span>
-                </div>
-            
-                <div className="w-full ml-4 flex-col justify-center items-center space-y-2 ">
-                    <Link className="block transition ease-in-out delay-100 hover:font-bold hover:scale-105 " to="/about-us"> About </Link>
-                    <Link className="block transition ease-in-out delay-100 hover:font-bold hover:scale-105 " to="/blog"> Blog </Link>
-                    <Link className="block transition ease-in-out delay-100 hover:font-bold hover:scale-105 " to="/contact"> Contact</Link>
-                    <Link className="block transition ease-in-out delay-100 hover:font-bold hover:scale-105 " to="/explore"> Explore</Link>
-                </div>
-        </div>
+        <AnimatePresence>
+            {dropdownOpen ?
+            <motion.div 
+                initial={{ opacity: 0}}
+                animate= {{ opacity: 1}}
+                exit={{opacity:0, height:0}}
+                transition={{ease: "easeInOut", duration: 0.5}}
+
+                className="w-full h-auto z-20 bg-white" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)'}}>
+                    <div className="w-full py-2 px-4 flex justify-between  items-center">
+                        <Link to="/login" className="h-auto flex  text-xs md:text-md items-start text-start font-medium text-purple-600 ">Get Started</Link>
+                        <span className="w-8 h-8 border-2 border-black rounded-full p-1">
+                            <UserIcon className="w-full h-full text-black" />
+                        </span>
+                    </div>
+                
+                    <div className="w-full ml-4 flex-col justify-center items-center space-y-1 ">
+                        <Link className="block transition py-2 ease-in-out delay-100 hover:font-bold hover:scale-105 " to="/about-us"> About </Link>
+                        <Link className="block transition py-2 ease-in-out delay-100 hover:font-bold hover:scale-105 " to="/blog"> Blog </Link>
+                        <Link className="block transition py-2 ease-in-out delay-100 hover:font-bold hover:scale-105 " to="/contact"> Contact</Link>
+                        <Link className="block transition py-2 ease-in-out delay-100 hover:font-bold hover:scale-105 " to="/explore"> Explore</Link>
+                    </div>
+            </motion.div>
+            : ""}
+        </AnimatePresence>
 
 
 

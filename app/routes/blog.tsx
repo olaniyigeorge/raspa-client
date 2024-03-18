4ffff4063adbe3729ba92f1110e4c43c274abc01
unknown <olaniyigeorge77@gmail.com>
2024-03-18 04:27:56 +0100

    Testing animation

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,7 +1,26 @@
+import { motion } from "framer-motion";
+
 export default function Blog() {
 
 
-    return <div className="p-10 text-3xl">
-        Blog
+    return <div className="p-10 flex justify-around gap-4 w-full h-screen items-center text-3xl">
+        <motion.div
+            initial={{ scale: 0 }}
+            animate= {{ scale: [1.1,1]}}
+            exit={{opacity:0}}
+            transition={{ease: "easeInOut", duration: 1}}
+        className="bg-red-500 flex text-white justify-center items-center rounded-lg h-full w-1/2">
+            Blog
+        </motion.div>  
+
+        <motion.div
+            initial={{ scale: 0 }}
+            animate= {{ scale: [1.7,1]}}
+            exit={{opacity:0}}
+            transition={{ease: "easeInOut", duration: 1}}
+            className="bg-purple- flex text-white justify-center items-center rounded-lg h-full w-1/2"
+        >
+            <img src={`images/rasp-logo-purple.png`} alt="logo" />
+        </motion.div>
     </div>
 }

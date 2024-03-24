0f4d4ff0cf10156d060755077ab762e5cee7e756
unknown <olaniyigeorge77@gmail.com>
2024-03-24 23:13:20 +0100

    Added animated scroll down component

---
+++ app/components/scroll-down.tsx
@@ -0,0 +1,29 @@
+import { ChevronDownIcon } from "@heroicons/react/24/solid";
+import React, { useEffect } from "react";
+
+interface ScrollDownAnimationProps {
+  scrollTopValue: number;
+}
+
+export default function ScrollDownAnimation(props: ScrollDownAnimationProps) {
+    const { scrollTopValue }= props
+  const scrollDown = () => {
+    window.scrollBy({
+      top: scrollTopValue,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", scrollDown);
+    };
+  }, [scrollTopValue]);
+
+  return (
+    <button onClick={scrollDown} className="">
+        <ChevronDownIcon className="w-8 h-8 flex text-purple-600 border-2 rounded-full border-white items-center" />
+    </button>
+  );
+};

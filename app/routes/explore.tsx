cdfd0e6d5aaeb8b17044389ae94fb3d69b960f10
unknown <olaniyigeorge77@gmail.com>
2024-03-24 13:57:20 +0100

    Placing properties and explore maps side by side

--- app/routes/explore.tsx
+++ app/routes/explore.tsx
@@ -1,20 +1,35 @@
 import { Link } from "@remix-run/react";
+import { motion } from "framer-motion";
 import Header from "~/components/header";
+import PropertyCard, { akure_property, IProperty } from "~/components/property-card";
 
 export default function Explore() {
 
 
     return <div className="">
         <Header mode={"light"} />
-        <span className="w-full flex justify-between">
-            <h1 className="text-3xl">
-                Explore
-            </h1>
 
-            <Link to="/"> Go home</Link>
-        </span>
+        <div className="flex p-2 ">
+            <div className="md:flex md:flex-grow md:space-x-3 md:items-start w-3/5 px-2 h-auto">
+                    {akure_property.map((property: IProperty) => (
+                        <PropertyCard key={property.address} {...property} />
+                    ))}
+            </div>
+
+            <motion.div
+            initial={{ scale: 0 }}
+            animate= {{ scale: [1.7,1]}}
+            exit={{opacity:0}}
+            transition={{ease: "easeInOut", duration: 1}}
+            className="bg-purple- flex w-2/5 text-white justify-center items-center rounded-lg h-full "
+            >
+                <img src={`images/rasp-logo-purple.png`} alt="logo" />
+            </motion.div>
+
+
+        </div>
+
         
-
 
         <Link to="/explore/map" > Explore properties on map</Link>    
     </div>

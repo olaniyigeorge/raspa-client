107d74dcf568694f2acc638d8ef34380a60b0f47
unknown <olaniyigeorge77@gmail.com>
2024-03-24 23:15:17 +0100

    Setting up harvets property data

---
+++ app/components/harvested-data.tsx
@@ -0,0 +1,16 @@
+export const zalasoft = {
+    "name": "A Plot of Land at Ileyo, Pelebe opposite Jaja Hotel, Oda road",
+    "address": "Ileyo, Pelebe opposite Jaja Hotel, Oda road",
+    "description": "A Plot of Land at Ileyo, Pelebe opposite Jaja Hotel, Oda road",
+    "title": "Family Receipt",
+    "price": "N600k",
+    "contact": "+2349060693929",
+    "latitude": null,
+    "longitude": null,
+    "size": null,
+    "listed_by": "zalasoft",
+    "owner": "default_owner",
+    "created_at": "Jun 6, 2021 20:41",
+    "updated_at": "Jun 6, 2021 20:41"
+}
+  

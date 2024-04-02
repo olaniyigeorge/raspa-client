82682b2ff133519f674fa8850462725f90d4cd2b
unknown <olaniyigeorge77@gmail.com>
2024-04-02 22:18:52 +0100

    Blog hero description and articles container

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -6,15 +6,16 @@ export default function Blog() {
 
     return <>
     <Header mode={'light'} />
-    <div className="p-10 flex justify-around gap-4 w-full h-auto items-center text-3xl">
+    <div className="p-2 md:p-10 md:flex justify-around gap-4 w-full h-auto items-center text-3xl">
         <motion.div
-            initial={{ scale: 0 }}
-            animate= {{ scale: [1.1,1]}}
-            exit={{opacity:0}}
+            initial={{ scale: 0, opacity:0 }}
+            animate= {{ scale: [0.9,1.1,1], opacity:1}}
+            exit={{opacity:0, }}
             transition={{ease: "easeInOut", duration: 1}}
-            className="w-1/2 text-7xl  text-gray-900 font-extrabold flex justify-center items-center rounded-lg h-full ">
+            className="w-full md:w-1/2 text-5xl md:text-5xl  text-gray-900 font-extrabold flex justify-center items-center rounded-lg h-full "
+        >
             <h1 className="tracking-tighter leading-tight">
-                Stay Informed, Stay Ahead: Explore Our <span className="text-purple-700">Real Estate</span> Blog
+                Stay Informed, Stay Ahead: Explore Our <span className="text-purple-700">Real Estate Blog</span> 
             </h1>
         </motion.div>  
 
@@ -23,10 +24,15 @@ export default function Blog() {
             animate= {{ scale: [1.7,1]}}
             exit={{opacity:0}}
             transition={{ease: "easeInOut", duration: 1}}
-            className="bg-purple- flex text-white justify-center items-center rounded-lg h-full w-1/2"
+            className="w-full md:w-1/2 flex text-white justify-center items-center rounded-lg h-full w-1/2"
         >
             <img src={`images/rasp-logo-purple.png`} alt="logo" />
         </motion.div>
+    </div>
+
+    <div className="w-full h-[600px] rounded-lg border">
+
+
     </div>
     </>
 }

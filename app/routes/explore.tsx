234c3c040afd432a3337d38f00fa44e7d08f901e
unknown <olaniyigeorge77@gmail.com>
2024-04-02 22:17:53 +0100

    Insigs'

--- app/routes/explore.tsx
+++ app/routes/explore.tsx
@@ -9,7 +9,7 @@ export default function Explore() {
     return <div className="">
         <Header mode={"light"} />
 
-        <div className="flex p-2 ">
+        <div id="properties" className="flex p-2 ">
             <div className="md:flex md:flex-grow md:space-x-3 md:items-start w-3/5 px-2 h-auto">
                     {akure_property.map((property: IProperty) => (
                         <PropertyCard key={property.address} {...property} />
@@ -17,11 +17,11 @@ export default function Explore() {
             </div>
 
             <motion.div
-            initial={{ scale: 0 }}
-            animate= {{ scale: [1.7,1]}}
-            exit={{opacity:0}}
-            transition={{ease: "easeInOut", duration: 1}}
-            className="bg-purple- flex w-2/5 text-white justify-center items-center rounded-lg h-full "
+                initial={{ scale: 0 }}
+                animate= {{ scale: [1.7,1]}}
+                exit={{opacity:0}}
+                transition={{ease: "easeInOut", duration: 1}}
+                className="bg-purple-200 flex w-2/5 text-white justify-center items-center rounded-lg h-full "
             >
                 <img src={`images/rasp-logo-purple.png`} alt="logo" />
             </motion.div>

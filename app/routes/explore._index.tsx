93ca17f94ff3ee0aff94a207615fe68969fb687c
unknown <olaniyigeorge77@gmail.com>
2024-04-04 17:47:17 +0100

    Added filter section

--- app/routes/explore._index.tsx
+++ app/routes/explore._index.tsx
@@ -10,7 +10,20 @@ export default function ExploreIndex() {
     return (
         <>
             <div className="mt-16">
-                <div id="properties" className="flex p-2">
+                <div id="listing-filters" className="">
+                    <h1 className="">
+                        Filter uour search result
+                    </h1>
+
+                    <section className="p-2">
+                        <input type="text" placeholder="Search properties" className="border border-white"/>
+                        <button className="border bg-white text-gray-800 p-2"> Houses and Apartments </button>
+                        <button className="border bg-white text-gray-800 p-2"> Location </button>
+                        <button className="border bg-white text-gray-800 p-2"> Min Price </button>
+                        <button className="border bg-white text-gray-800 p-2"> Max Price </button>
+                    </section>
+                </div>
+                <div id="properties" className="bg-white flex p-2">
                     <div className="md:flex md:flex-grow md:space-x-3 md:items-start w-3/5 px-2 h-auto">
                         {/* Map over akure_property array and render PropertyCard components */}
                         {/* Make sure akure_property is defined and is an array of IProperty objects */}

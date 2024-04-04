93a2df34443684837a2a4fd70783f92dc6656e22
unknown <olaniyigeorge77@gmail.com>
2024-04-04 14:24:42 +0100

    Layout

--- app/routes/explore.tsx
+++ app/routes/explore.tsx
@@ -1,36 +1,51 @@
-import { Link } from "@remix-run/react";
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/solid";
+import { Link, Outlet } from "@remix-run/react";
 import { motion } from "framer-motion";
+import { useState } from "react";
 import Header from "~/components/header";
 import PropertyCard, { akure_property, IProperty } from "~/components/property-card";
+import Sidebar from "~/components/sidebar";
 
 export default function Explore() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+      };
+      
+    return <>
+        <section className="flex w-full duration-500">
+          <Sidebar
+            isOpen={isSidebarOpen}
+            toggleSidebar={toggleSidebar}
+          />
+    
+          <section
+            className={`flex flex-col flex-grow relative h-screen duration-500 ${
+              isSidebarOpen ? "" : ""
+            }`}
+          >
+            <section className="w-full flex transparent absolute z-20 top-0 left-0 items-center justify-between p-1">
+                <Header mode="light" />
+              <div 
+                className="w-fit h-fit bg-purple-600 text-white opacity-100 rounded-lg p-2 hover:bg-purple-700"
+                onClick={toggleSidebar}
+              >
+                {isSidebarOpen ? <ChevronDownIcon className="w-5 h-5 text-white"/>: <ChevronUpIcon className="w-5 h-5 text-white"/>}
+                </div>
+            </section>
+    
+            <section className=" bg-slate-100 top-0 left-0 relative text-purple-600 flex flex-1 justify-start items-start">
+              <Outlet />
+            </section>
+          </section>
+        </section>
+      
+      </>
 
-    return <div className="">
-        <Header mode={"light"} />
-
-        <div id="properties" className="flex p-2 ">
-            <div className="md:flex md:flex-grow md:space-x-3 md:items-start w-3/5 px-2 h-auto">
-                    {akure_property.map((property: IProperty) => (
-                        <PropertyCard key={property.address} {...property} />
-                    ))}
-            </div>
-
-            <motion.div
-                initial={{ scale: 0 }}
-                animate= {{ scale: [1.7,1]}}
-                exit={{opacity:0}}
-                transition={{ease: "easeInOut", duration: 1}}
-                className="bg-purple-200 flex w-2/5 text-white justify-center items-center rounded-lg h-full "
-            >
-                <img src={`images/rasp-logo-purple.png`} alt="logo" />
-            </motion.div>
-
-
-        </div>
 
         
 
-        <Link to="/explore/map" > Explore properties on map</Link>    
-    </div>
+    //     <Link to="/explore/map" > Explore properties on map</Link>    
+    // </div>)
 }

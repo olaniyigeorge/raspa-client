9ffdd90af4dd0cba4169278c6ebc6af05ffc04f8
unknown <olaniyigeorge77@gmail.com>
2024-04-06 16:25:23 +0100

    Pages

---
+++ app/routes/property_.$propID_.payment-plans.installments.tsx
@@ -0,0 +1,22 @@
+import { LoaderFunctionArgs } from "@remix-run/node"
+
+
+export default function PaymentPlanInstallements() {
+
+
+    return <>
+        <div className="bg-blue-600 w-10 h-10">
+
+        </div>
+    
+    </>
+
+}
+
+
+
+export async function loader({request,}: LoaderFunctionArgs) {
+
+
+    return {}
+}

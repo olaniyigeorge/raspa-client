57b6d3034b4b87fce82620096ae10c272b4f4f47
unknown <olaniyigeorge77@gmail.com>
2024-04-06 16:15:33 +0100

    Sale page

---
+++ app/routes/property_.$propID_.payment-plans.buy-now.tsx
@@ -0,0 +1,22 @@
+import { LoaderFunctionArgs } from "@remix-run/node"
+
+
+export default function PaymentPlanBuyNow() {
+
+
+    return <>
+        <div className="bg-red-500 w-10 h-10">
+
+        </div>
+    
+    </>
+
+}
+
+
+
+export async function loader({request,}: LoaderFunctionArgs) {
+
+
+    return {}
+}

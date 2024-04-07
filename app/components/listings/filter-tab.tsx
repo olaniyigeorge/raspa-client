373f9218879762ecd01d3b08211937e7c040baf9
unknown <olaniyigeorge77@gmail.com>
2024-04-07 04:12:15 +0100

    Construct listing filter endpoint

--- app/components/listings/filter-tab.tsx
+++ app/components/listings/filter-tab.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 
 
 export default function ListingsFilter() {
     const [selectedAction, setSelectedAction] = useState<string>('sale')
+    const [searchQuery, setSearchQuery] = useState<string>('')
+    const [minPrice, setMinPrice] = useState<number>(0)
+    const [maxPrice, setMaxPrice] = useState<number>(500000)
+    const [size, setSize] = useState<number>()
+
 
     const baseEndpoint = "/api/listings/?property__size=&price=&property__features__name=&property__type=&property__features__count=&listing_type=&price__lte=&price__gte="
 
@@ -12,15 +17,30 @@ export default function ListingsFilter() {
     // Gets filter options and generates endpoint
     // search-query, city(location), action(rent,sale,invest), size, min-price, max-price
 
-
+    const filterEndpoint = `/api/listings/?property__size=${size}&price=${""}&property__features__name=${""}&property__type=${""}&property__features__count=${""}&listing_type=${selectedAction}&price__lte=&${""}price__gte=${""}`
     const handleButtonClick = (button: string) => {
         setSelectedAction(button);
       };
 
+    useEffect(() => {
+
+    const filterEndpoint = `/api/listings/?property__size=${size}&price=${""}&property__features__name=${""}&property__type=${""}&property__features__count=${""}&listing_type=${selectedAction}&price__lte=&${""}price__gte=${""}`
+
+    }, [selectedAction, searchQuery, minPrice, maxPrice, size])
 
     return <>
+
+      <div id="listing-filters" className="p-4 text-sm md:text-md ">
+        <span className="w-full flex space-x-3 justify-start items-center">
+          <h1 className="text-xl font-medium text-gray-900">
+            Filter your search result
+          </h1>
+
+          <p className="text-xs">{filterEndpoint}</p>
+        </span>
+            
         <section className=" w-full text-gray-800  border-b border-gray-200 py-1 md:flex justify-start space-y-1 md:space-y-0 md:space-x-10 items-center ">
-            <input type="text" placeholder="Search properties" className="border  border-red-500 focus:outline-purple-600 p-2 rounded h-10 w-full md:w-[400px] border-white"/>
+            <input type="text" placeholder="Search properties" className="border  border-purple-300 focus:outline-purple-600 p-2 rounded h-10 w-full md:w-[400px] "/>
             
 
             <div className="  flex justify-center mt-4">
@@ -68,5 +88,23 @@ export default function ListingsFilter() {
                 <button className="rounded-md border bg-white p-2"> Max Price </button>
             </div>
         </section>
+      </div>
     </>
+}
+
+
+interface filterParams {
+  property__size: string;
+  price?: string;
+  property__features__name: string;
+  property__type: string;
+  property__features__count: string;
+  listing_type: string;
+  price__lte: string;
+  price__gte: string;
+
+}
+
+function constructFilterEndpoint(props: filterParams) {
+  
 }

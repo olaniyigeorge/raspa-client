f9609a11faee3d01d5b561c903fecf0e33445015
unknown <olaniyigeorge77@gmail.com>
2024-04-07 15:11:07 +0100

    ----

--- app/components/listings/listings-container.tsx
+++ app/components/listings/listings-container.tsx
@@ -1,8 +1,5 @@
 import PropertyCard, { akure_property, IProperty } from "../property-card";
 
-
-
-
 export default function ListingContainier(props: IProperty[]) {
 
     // const listings = {...props}
@@ -11,12 +8,15 @@ export default function ListingContainier(props: IProperty[]) {
 
 
     return <div className="w-full md:w-4/5 md:grid grid-cols-3 gap-3 justify-between">
-    {/* Map over akure_property array and render PropertyCard components      md:flex md:flex-grow md:space-x-3 md:items-start w-3/5 px-2 h-auto */}
-    {/* Make sure akure_property is defined and is an array of IProperty objects */}
-    {/* Example: */}
-    {akure_property.map((property: IProperty) => (
-        <PropertyCard key={property.address} {...property} />
-    ))} 
+    {/* Map over akure_property array and render PropertyCard components      md:flex md:flex-grow md:space-x-3 md:items-start w-3/5 px-2 h-auto 
+     Make sure akure_property is defined and is an array of IProperty objects
+     Example: */}
+    
+    {
+        akure_property.map((property: IProperty) => (
+            <PropertyCard key={property.address} {...property} />
+        ))
+    } 
 
-</div>
+    </div>
 }

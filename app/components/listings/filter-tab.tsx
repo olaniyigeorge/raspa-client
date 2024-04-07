9732608615685567487078d39af56504deaa6ea5
unknown <olaniyigeorge77@gmail.com>
2024-04-07 15:10:56 +0100

    ----

--- app/components/listings/filter-tab.tsx
+++ app/components/listings/filter-tab.tsx
@@ -1,3 +1,4 @@
+import { ChevronUpIcon, ChevronDownIcon } from "@heroicons/react/24/solid";
 import { useEffect, useState } from "react";
 
 
@@ -6,25 +7,31 @@ import { useEffect, useState } from "react";
 export default function ListingsFilter() {
     const [selectedAction, setSelectedAction] = useState<string>('sale')
     const [searchQuery, setSearchQuery] = useState<string>('')
-    const [minPrice, setMinPrice] = useState<number>(0)
-    const [maxPrice, setMaxPrice] = useState<number>(500000)
-    const [size, setSize] = useState<number>()
+    const [size, setSize] = useState<number | string>()
+    const [minPrice, setMinPrice] = useState<number | any>("")
+    const [maxPrice, setMaxPrice] = useState<number | any >("")
+    
 
 
-    const baseEndpoint = "/api/listings/?property__size=&price=&property__features__name=&property__type=&property__features__count=&listing_type=&price__lte=&price__gte="
+    const [minPriceDropDown, setminPriceDropDown] = useState<boolean>(false)
+    const [maxPriceDropDown, setmaxPriceDropDown] = useState<boolean>(false)
+    const [sizeInputVisible, toggleSizeInput] = useState<boolean>(false)
+
+
+    const baseEndpoint = "/api/listings/?search=&property__size=&price=&property__features__name=&property__type=&property__features__count=&listing_type=&price__lte=&price__gte="
 
 
     // Gets filter options and generates endpoint
     // search-query, city(location), action(rent,sale,invest), size, min-price, max-price
 
-    const filterEndpoint = `/api/listings/?property__size=${size}&price=${""}&property__features__name=${""}&property__type=${""}&property__features__count=${""}&listing_type=${selectedAction}&price__lte=&${""}price__gte=${""}`
+    const filterEndpoint = `/api/listings/?search=${searchQuery}&property__size=${size}&price=${""}&property__features__name=${""}&property__type=${""}&property__features__count=${""}&listing_type=${selectedAction}&price__lte=&${maxPrice}price__gte=${minPrice}`
     const handleButtonClick = (button: string) => {
         setSelectedAction(button);
       };
 
     useEffect(() => {
 
-    const filterEndpoint = `/api/listings/?property__size=${size}&price=${""}&property__features__name=${""}&property__type=${""}&property__features__count=${""}&listing_type=${selectedAction}&price__lte=&${""}price__gte=${""}`
+    const filterEndpoint = `/api/listings/?search=&property__size=${size}&price=${""}&property__features__name=${""}&property__type=${""}&property__features__count=${""}&listing_type=${selectedAction}&price__lte=&${""}price__gte=${""}`
 
     }, [selectedAction, searchQuery, minPrice, maxPrice, size])
 
@@ -40,27 +47,11 @@ export default function ListingsFilter() {
         </span>
             
         <section className=" w-full text-gray-800  border-b border-gray-200 py-1 md:flex justify-start space-y-1 md:space-y-0 md:space-x-10 items-center ">
-            <input type="text" placeholder="Search properties" className="border  border-purple-300 focus:outline-purple-600 p-2 rounded h-10 w-full md:w-[400px] "/>
+            <input type="text" onChange={(e) => {setSearchQuery(e.target.value)}} placeholder="Search properties" className="border  border-purple-300 focus:outline-purple-600 p-2 rounded h-10 w-full md:w-[400px] "/>
             
 
             <div className="  flex justify-center mt-4">
-              <div className=" p-1 flex items-center space-x-2 rounded-full" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)'}}>
-                <button
-                  className={`p-2 flex-grow  justify-center items-center rounded-full transition-all ${
-                    selectedAction === 'sale' ? 'bg-purple-600 text-white shadow-md font-medium' : ' '
-                  }`}
-                  onClick={() => handleButtonClick('sale')}
-                >
-                  Buy
-                </button>
-                <button
-                  className={`p-2 flex-grow justify-center items-center rounded-full transition-all ${
-                    selectedAction === 'SALE' ? 'bg-purple-600 text-white shadow-md font-medium' : ''
-                  }`}
-                  onClick={() => handleButtonClick('SALE')}
-                >
-                  Sell
-                </button>
+              <div className=" p-1 flex items-center space-x-2 rounded-full" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)'}}>            
                 <button
                   className={`p-2  flex-grow  justify-center items-center rounded-full transition-all ${
                     selectedAction === 'rent' ? 'bg-purple-600 text-white shadow-md font-medium' : ''
@@ -69,6 +60,14 @@ export default function ListingsFilter() {
                 >
                   Rent
                 </button>
+                <button
+                  className={`p-2 flex-grow  justify-center items-center rounded-full transition-all ${
+                    selectedAction === 'sale' ? 'bg-purple-600 text-white shadow-md font-medium' : ' '
+                  }`}
+                  onClick={() => handleButtonClick('sale')}
+                >
+                  Sale
+                </button> 
                 <button
                   className={`p-2 flex-grow  justify-center items-center rounded-full transition-all ${
                     selectedAction === 'invest' ? 'bg-purple-600 text-white shadow-md font-medium' : ''
@@ -83,9 +82,58 @@ export default function ListingsFilter() {
 
             <div className=" flex justify-start space-x-3 md:space-x-10 w-full ">
                 <button className="rounded-md border bg-white p-2 line-clamp-1"> Houses/Land  </button>
-                <button className="rounded-md border bg-white p-2"> Location </button>
-                <button className="rounded-md border bg-white p-2"> Min Price </button>
-                <button className="rounded-md border bg-white p-2"> Max Price </button>
+                <button className="rounded-md border  p-2" onClick={() => {toggleSizeInput(!sizeInputVisible)}}>
+                  {sizeInputVisible? <>
+                    <input type="number" className="w-[12px] auto-focus focus:outline-none" onChange={(e) => {setSize(e.target.value)}} />
+                    </>:<p>Size</p>}  </button>
+
+
+                <button className="rounded-md border bg-white p-2 relative">
+                  <div className="w-full flex justify-start space-x-2 items-center">
+                    <span className={` justify-start items-center ${minPriceDropDown ? "hidden": "flex"}`}>Min Price</span>
+  
+                    <span className={`justify-start items-center  ${minPriceDropDown ? "flex": "hidden"}`}>
+                      <input type="number" className="w-[12px] focus:outline-none" onChange={(e) => {setMinPrice(e.target.value)}} />
+                    </span>
+                    <ChevronDownIcon onClick={() => {setminPriceDropDown(!minPriceDropDown)}} className="w-4 h-4" />
+                  </div>
+                  
+              
+
+                  {minPriceDropDown? (
+                    <div className="absolute transform duration-500 flex flex-col justify-start translate-y-0 border bg-white z-20 rounded top-10 right-0 w-full">
+                      <p onClick={() => {setMinPrice(100000)}}>100000</p>
+                      <p onClick={() => {setMinPrice(200000)}}>200000</p>
+                      <p onClick={() => {setMinPrice(300000)}}>300000</p>
+                      <p onClick={() => {setMinPrice(15000000)}}>15000000</p>
+                      <p onClick={() => {setMinPrice(30000000)}}>30000000</p>                    
+                    </div>
+                  ): ""}
+                  
+                   
+                </button>
+                <button className="rounded-md border bg-white p-2 relative">
+                  <div className="w-full flex justify-start space-x-2 items-center">
+                    <span className={` justify-start items-center ${maxPriceDropDown ? "hidden": "flex"}`}>Max Price</span>
+  
+                    <span className={`justify-start items-center  ${maxPriceDropDown ? "flex": "hidden"}`}>
+                      <input type="number" className="w-auto focus:outline-none" onChange={(e) => {setMaxPrice(e.target.value)}} />
+                    </span>
+                    <ChevronUpIcon onClick={() => {setmaxPriceDropDown(!maxPriceDropDown)}} className="w-4 h-4" />
+                  </div>
+
+                  {maxPriceDropDown? (
+                    <div className="absolute transform duration-500 flex flex-col justify-start translate-y-0 border bg-white z-20 rounded top-10 right-0 w-full">
+                      <p onClick={() => {setMaxPrice(minPrice*1.5)}}>{minPrice*1.5}</p>
+                      <p onClick={() => {setMaxPrice(200000)}}>200000</p>
+                      <p onClick={() => {setMaxPrice(75000000)}}>75 million</p>
+                      <p onClick={() => {setMaxPrice(150000000)}}>150 million</p>
+                      <p onClick={() => {setMaxPrice("")}}>No cap</p>                    
+                    </div>
+                  ): ""}
+                  
+                   
+                </button>
             </div>
         </section>
       </div>
@@ -106,5 +154,5 @@ interface filterParams {
 }
 
 function constructFilterEndpoint(props: filterParams) {
-  
+  throw new Error("Not Implemented")
 }

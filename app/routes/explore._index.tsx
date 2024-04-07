d5d63ed57425bef88fe82fb00d91c9f61f8546e4
unknown <olaniyigeorge77@gmail.com>
2024-04-07 04:14:15 +0100

    Added Filter tab hero text into comp

--- app/routes/explore._index.tsx
+++ app/routes/explore._index.tsx
@@ -15,13 +15,8 @@ export default function ExploreIndex() {
             transition={{ ease: "easeIn", duration: 1 }}
         >
             <div className="mt-16">
-                <div id="listing-filters" className="p-4 text-sm md:text-md ">
-                    <h1 className="text-xl font-medium text-gray-900">
-                        Filter your search result
-                    </h1>
-
-                    <ListingsFilter />
-                </div>
+                <ListingsFilter />
+            
                 <div id="properties" className="w-full md:flex space-x-2 px-2">
                     <ListingContainier {...akure_property} />
                     
@@ -35,15 +30,7 @@ export default function ExploreIndex() {
                         src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA7qx8ah0ZVIv43KxUPPspBRG1-fwY6jOU
                             &q=Akure,+Ondo+state+NG">
                     </iframe>
-                    {/* <motion.div
-                        // initial={{ scale: 0 }}
-                        // animate={{ scale: [1.7, 1] }}
-                        // exit={{ opacity: 0 }}
-                        // transition={{ ease: "easeInOut", duration: 1 }}
-                        className="bg-purple-200 flex w-2/5 text-white justify-center items-center rounded-lg h-full"
-                    >
-                        <img src="images/rasp-logo-purple.png" alt="logo" />
-                    </motion.div> */}
+
                 </div>
             </div>
         </motion.div>

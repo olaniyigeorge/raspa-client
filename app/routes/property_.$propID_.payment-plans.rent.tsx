164d4b3fa1dbcdee9a62c3d67281add6a12d57a0
unknown <olaniyigeorge77@gmail.com>
2024-04-10 13:20:36 +0100

    Payment shell for rent

--- app/routes/property_.$propID_.payment-plans.rent.tsx
+++ app/routes/property_.$propID_.payment-plans.rent.tsx
@@ -1,17 +1,29 @@
 import { LoaderFunctionArgs } from "@remix-run/node"
-import { useParams } from "@remix-run/react"
+import { Form, Link, useParams } from "@remix-run/react"
 
 export default function PaymentPlanRent() {
     const params = useParams()
 
     return <>
 
-        <div className="w-full h-screen flex justify-center items-center ">
-            <div className="bg-green-500 w-1/2 h-[500px] rounded-lg ">
-                <p className="p-10 text-white"> Rent {params.propID}</p>
+
+        <div className="w-full h-screen bg-purple-50 flex justify-center items-center ">
+            <div className="bg-white shadow flex w-1/2 h-[500px] rounded-lg ">
+                <div className="w-1/2 flex flex-col p-10 border-r">
+                    <div className="w-full h-1/2">Images</div>
+                    <div className="w-full h-1/2">Property Info: Invest in {params.propID} </div>
+                </div>
+
+                <div className="w-1/2 p-2">
+                    <Form method="post">
+                        <button className="rounded-md py-1 px-2 bg-red-500 text-white">Rent {params.propID}</button>
+                        <Link to="" className="rounded-md py-1 px-2 bg-gray-200 text-gray-800">Pay in installments</Link>
+                    </Form>
+                    
+                </div>
+                
             </div>
         </div>
-
     
     </>
 

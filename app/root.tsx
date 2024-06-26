a0c9ff37577e606e1669b3d1e9b3640b9883080f
unknown <olaniyigeorge77@gmail.com>
2024-06-26 01:43:54 +0100

    Implemented token refresh

--- app/root.tsx
+++ app/root.tsx
@@ -52,6 +52,12 @@ export default function App() {
 
 export async function loader({request,}: LoaderFunctionArgs){
     console.log("Loading.......")
+    const ENVT = {
+      APP_COOKIE_NAME: process.env.APP_COOKIE_NAME,
+      GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+      GOOGLE_MAP_ID: process.env.GOOGLE_MAP_ID,
+      APP_SECRET: process.env.APP_SECRET,
+    }
     
     let ctx: UserContext | null = null;
     const current_token = await currentToken(request);
@@ -71,28 +77,20 @@ export async function loader({request,}: LoaderFunctionArgs){
 
     console.log("Context Response: ", ctx_response.status)
     if (ctx_response.status == 200) {
-      ctx = json(ctx_response.body)
+      ctx = ctx_response.body
     }
     else if (ctx_response.status == 401) {
 
       console.log("Attempting token refresh.........")
       const refreshToken = await currentRefreshToken(request);
 
-      console.log("RT: ", refreshToken)
+      // console.log("RT: ", refreshToken)
 
       if (refreshToken && refreshToken !== 'anonymous') {
-        // const refresh_args: fetcherProps = {
-        //   endpoint: getUrl('sign-in-refresh'), 
-        //   method: 'POST',
-        //   body:  JSON.stringify({'refresh': refreshToken}),
-        // }
-        // const refresh_response = await fetchData(refresh_args);
         const fetchNewAccessResponse = await fetchNewAccessToken(refreshToken);
 
-        console.log("New token: ", fetchNewAccessResponse)
-
         if (fetchNewAccessResponse === null) {
-          return redirect("");
+          return json({ ctx, ENVT})
         }
 
         //Use the new access token in subsequent requests if needed
@@ -106,15 +104,39 @@ export async function loader({request,}: LoaderFunctionArgs){
             "Set-Cookie": await storage.commitSession(session),
           },
         });
-      } 
+      }
+      // if (refreshToken && refreshToken !== 'anonymous') {
+      //   // const refresh_args: fetcherProps = {
+      //   //   endpoint: getUrl('sign-in-refresh'), 
+      //   //   method: 'POST',
+      //   //   body:  JSON.stringify({'refresh': refreshToken}),
+      //   // }
+      //   // const refresh_response = await fetchData(refresh_args);
+      //   const fetchNewAccessResponse = await fetchNewAccessToken(refreshToken);
+
+      //   console.log("New token: ", fetchNewAccessResponse)
+
+      //   if (fetchNewAccessResponse === null) {
+      //     return redirect("");
+      //   }
+
+      //   //Use the new access token in subsequent requests if needed
+      //   const session = await enableAuthenticableServerApiRequests(request, {
+      //     token: fetchNewAccessResponse,
+      //     refresh: refreshToken,
+      //   });
+
+      //   return redirect("/", {
+      //     headers: {
+      //       "Set-Cookie": await storage.commitSession(session),
+      //     },
+      //   });
+      // } 
+    }
+    else {
+      ctx = null
     }
 
-    const ENVT = {
-      APP_COOKIE_NAME: process.env.APP_COOKIE_NAME,
-      GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
-      GOOGLE_MAP_ID: process.env.GOOGLE_MAP_ID,
-      APP_SECRET: process.env.APP_SECRET,
-    }
 
     return json({ ctx, ENVT})
 }
